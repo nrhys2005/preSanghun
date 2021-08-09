6683ff974fe62d6feaa30a35123977d50f9ec456
이상훈 <isanghun@isanghun-ui-MacBookPro.local>
2021-08-09 20:36:22 +0900

    server setting

---
+++ src/main/java/com/sanghun/sanghun/SanghunApplication.java
@@ -0,0 +1,13 @@
+package com.sanghun.sanghun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanghunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanghunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanghun/sanghun/SanghunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanghun.sanghun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanghunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
